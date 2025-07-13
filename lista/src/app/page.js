af8730aae9c2b4dfa4703941b7f3093ee10663db
marcelavasquez11 <marcelavax@hotmail.com>
2025-07-13 10:46:53 -0600

    guia 3 lista

---
+++ lista/src/app/page.js
@@ -0,0 +1,16 @@
+import styles from "./page.module.css";
+import Form from "@/components/Form";
+export default function Home() {
+ return (
+ <main className={styles.main}>
+ <div className="App">
+ <div>
+ <p>
+ Aquí haremos nuestro TO-DO list
+ </p>
+<Form></Form>
+ </div>
+ </div>
+ </main>
+ );
+}
